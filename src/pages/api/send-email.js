2b1f759b8c4d42f58365ce160217248617666f98
Serzhan Kenesbek <serzhan.kenesbek1@gmail.com>
2023-04-14 22:59:18 +0200

    Update send-email.js

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -6,14 +6,14 @@ export default function (req, res) {
       port: 465,
       host: "smtp.gmail.com",
       auth: {
-        user: 'fintech@ibcms.de',
+        user: 'serzhanator@gmail.com',
         pass: process.env.password,
       },
       secure: true,
     })
     const mailData = {
-      from: 'bancilaa78@gmail.com',
-      to: 'ardut2004@gmail.com',
+      from: 'serzhanator@gmail.com',
+      to: 'fintech@ibcms.de',
       subject: `Message From ${req.body.name}`,
       text: req.body.message + " | Sent from: " + req.body.email,
       html: `<div>${req.body.message}</div><p>Sent from:
